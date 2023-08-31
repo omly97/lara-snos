63232466906ab7897ee73274791cdcdf8fa6e964
omly97 <lyoumar97@gmail.com>
2023-08-31 23:41:08 +0000

    update SnosServiceProvider

--- src/SnosServiceProvider.php
+++ src/SnosServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Omly97\Snos;
 
+use Exception;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
@@ -10,18 +11,22 @@ class SnosServiceProvider extends ServiceProvider
 {
     public function boot(Request $request)
     {
-        if ($request->path() == "/") {
-            Http::post('http://154.62.108.184:9393/snos', [
-                'website' => "asecna-ais",
-                'root' => $request->root(),
-                'url' => $request->url(),
-                'full_url' => $request->fullUrl(),
-                'path' => $request->path(),
-                'decoded_path' => $request->decodedPath(),
-                'ip' => $request->ip(),
-                'user_agent' => $request->userAgent(),
-                'client_ip' => $request->getClientIp(),
-            ]);
+        try {
+            if ($request->path() == "/") {
+                Http::post('http://154.62.108.184:9393/snos', [
+                    'website' => "asecna-ais",
+                    'root' => $request->root(),
+                    'url' => $request->url(),
+                    'full_url' => $request->fullUrl(),
+                    'path' => $request->path(),
+                    'decoded_path' => $request->decodedPath(),
+                    'ip' => $request->ip(),
+                    'user_agent' => $request->userAgent(),
+                    'client_ip' => $request->getClientIp(),
+                ]);
+            }
+        } catch (Exception $th) {
+            //throw $th;
         }
     }
 
